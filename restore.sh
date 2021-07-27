c1b84b1b0a5cf3ca05a59f5b0475345a74a55043
GayathriRameshK <87942448+GayathriRameshK@users.noreply.github.com>
2021-07-27 19:11:37 +0530

    Add files via upload

--- restore.sh
+++ restore.sh
@@ -20,16 +20,17 @@ mongorestore  --host="rs1/localhost:27019,localhost:27020" $FULL_DUMP_DIRECTORY
 echo "restore completed"
 for OPLOG in `ls $OPLOGS_DIRECTORY/*.bson.gz`; do
    OPLOG_TIMESTAMP=`echo $OPLOG | rev | cut -d "/" -f 1 | rev | cut -d "_" -f 1`
+   OPLOG_LIMIT= $OPLOG_TIMESTAMP:1
    if [ $OPLOG_TIMESTAMP -gt $FULL_DUMP_TIMESTAMP ]; then
       if [ $ALREADY_APPLIED_OPLOG -eq 0 ]; then
          ALREADY_APPLIED_OPLOG=1
          echo "applying oplog $LAST_OPLOG"
-         mongorestore  --host="rs1/localhost:27019,localhost:27020" --oplogFile $LAST_OPLOG --oplogReplay --dir /tmp/emptyDirForOpRestore --oplogLimit=$OPLOG_LIMIT
+         mongorestore  --host="rs1/localhost:27019,localhost:27020" --oplogFile $LAST_OPLOG --oplogReplay --dir $OPLOGS_DIRECTORY --oplogLimit=$OPLOG_LIMIT
          echo "applying oplog $OPLOG"
-         mongorestore --host="rs1/localhost:27019,localhost:27020" --oplogFile $OPLOG --oplogReplay --dir /tmp/emptyDirForOpRestore --oplogLimit=$OPLOG_LIMIT
+         mongorestore --host="rs1/localhost:27019,localhost:27020" --oplogFile $OPLOG --oplogReplay --dir $OPLOGS_DIRECTORY --oplogLimit=$OPLOG_LIMIT
       else
          echo "applying oplog $OPLOG"
-         mongorestore --host="rs1/localhost:27019,localhost:27020" --oplogFile $OPLOG --oplogReplay --dir /tmp/emptyDirForOpRestore --oplogLimit=$OPLOG_LIMIT
+         mongorestore --host="rs1/localhost:27019,localhost:27020" --oplogFile $OPLOG --oplogReplay --dir $OPLOGS_DIRECTORY --oplogLimit=$OPLOG_LIMIT
       fi
    else
       LAST_OPLOG=$OPLOG
